9796be808d309e93e88b506228aa2f3def1fee14
ingemar <>
2006-02-15 13:16:56 +0000

    New version

--- exps/steffe/ana_def.m
+++ exps/steffe/ana_def.m
@@ -2,13 +2,15 @@
 a_satch.clutter=[32 32 16];
 a_satch.sigma=4;
 %a_satch.plot=8;
-%a_satch.repair=-Inf;
+if expver>1
+  a_satch.repair=[104 29 0];
+end
 if strfind(data_path,'32p')
  analysis_do=0;
  analysis_txpower=8;
  d_saveintdir=result_path;
  d=strfind(d_saveintdir,'AUTO');
  if length(d)==1
-  d_saveintdir=[d_saveintdir(1:d-1) sprintf('%s_%s_%d@32p',datestr(analysis_start,29),name_expr,analysis_integr(1))];
+  d_saveintdir=[d_saveintdir(1:d-1) sprintf('%s_%s%d_%d@32p',datestr(analysis_start,29),name_expr,expver,analysis_integr(1))];
  end
 end
--- exps/steffe/t2ps.m
+++ exps/steffe/t2ps.m
@@ -1,41 +1,64 @@
-function dum=t2ps(site)
-load(['t_to_ps.txt.' site])
-l=size(t_to_ps,1)/2;
-td_t1=t_to_ps(1:l,1)';
-td_t2=t_to_ps(1:l,2)';
-td_am=t_to_ps(1:l,3)';
-td_ch=t_to_ps(1:l,4)';
-p_rep=640000;
+function dum=t2ps(site,rc)
+if nargin<2, rc=0; end
+t2psfile='t_to_ps.txt';
+if rc==0
+ apustr='';
+else
+ apustr=['_' int2str(rc)];
+end
+t2psfile=['t_to_ps.txt' apustr '.' lower(site)];
+t_to_ps=load(t2psfile,'-ascii');
 p_offsetppd=0;
-ch_adcint=[16 16 16];
-ch_filter={'w30d160.fir' 'w30d160.fir' 'w30d160.fir'};
-ch_f=[1 2 3];
-%for f=1:length(ch_f)
-% td_ch(find(td_ch==ch_f(f)))=f;
-%end
+if rc==2
+ td_t1=t_to_ps(:,1)';
+ td_t2=t_to_ps(:,2)';
+ td_am=t_to_ps(:,3)';
+ td_ch=t_to_ps(:,4)';
+ p_rep=300000;
+ ch_adcint=[15 15];
+ ch_filter={'b30d225.fir' 'b30d225.fir'};
+ ch_f=[1 2];
 
-rt1=rem(td_t1,10000);
-hh=find(td_ch==2 & (rt1<600 | rt1>2000));
-td_t1=[td_t1 td_t1(hh)];
-td_t2=[td_t2 td_t2(hh)];
-td_am=[td_am td_am(hh)];
-td_ch=[td_ch 4*ones(size(hh))];
+ cal=find(td_ch==3);
+ td_t1(cal)=td_t1(cal)+2;
+ td_ch(cal)=1;
+else
+ l=size(t_to_ps,1)/2;
+ td_t1=t_to_ps(1:l,1)';
+ td_t2=t_to_ps(1:l,2)';
+ td_am=t_to_ps(1:l,3)';
+ td_ch=t_to_ps(1:l,4)';
+ p_rep=640000;
+ ch_adcint=[16 16 16];
+ ch_filter={'w30d160.fir' 'w30d160.fir' 'w30d160.fir'};
+ ch_f=[1 2 3];
 
-rt1=rem(td_t1,10000);
-hh=find((td_ch==2 & rt1>2000) | (td_ch==3 & rt1<2000));
-td_t1(hh)=[];
-td_t2(hh)=[];
-td_am(hh)=[];
-td_ch(hh)=[];
+ rt1=rem(td_t1,10000);
+ hh=find(td_ch==2 & (rt1<600 | rt1>2000));
+ td_t1=[td_t1 td_t1(hh)];
+ td_t2=[td_t2 td_t2(hh)];
+ td_am=[td_am td_am(hh)];
+ td_ch=[td_ch 4*ones(size(hh))];
 
-cal=find(td_ch==3);
-td_t1(cal)=td_t1(cal)+2;
-d=find(td_t1(cal)>p_rep/4 & td_t1(cal)<p_rep/2);
-td_ch(cal(d))=2;
-d=find(td_t1(cal)>p_rep/2);
-td_ch(cal(d))=1;
+ rt1=rem(td_t1,10000);
+ hh=find((td_ch==2 & rt1>2000) | (td_ch==3 & rt1<2000));
+ td_t1(hh)=[];
+ td_t2(hh)=[];
+ td_am(hh)=[];
+ td_ch(hh)=[];
 
-d=find(td_ch==4);
-td_ch(d)=3;
-td_t1(d)=td_t1(d)+1;
-save(['steffe' site 'pat_PS'],'td_*','p_*','ch_*')
+ cal=find(td_ch==3);
+ td_t1(cal)=td_t1(cal)+2;
+ d=find(td_t1(cal)>p_rep/4 & td_t1(cal)<p_rep/2);
+ td_ch(cal(d))=2;
+ d=find(td_t1(cal)>p_rep/2);
+ td_ch(cal(d))=1;
+
+ d=find(td_ch==4);
+ td_ch(d)=3;
+ td_t1(d)=td_t1(d)+1;
+end
+
+name_expr='steffe';
+name_site=upper(site);
+save_PS
