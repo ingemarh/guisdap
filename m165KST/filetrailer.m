e11a6910df663ec0e6d3e75479662fe7810b39cb
ingemar <>
2006-02-27 13:24:58 +0000

    Cleaning m165

--- m165KST/filetrailer.m
+++
@@ -1,4 +0,0 @@
-% filetrailer.m: a dummy routine necessary for correct operation of ps-files
-% GUISDAP v1.60   96-05-27 Copyright Asko Huuskonen, Markku Lehtinen
-%
-% This routine intentionally left dummy   < >
--- m165KST/initialize.m
+++
@@ -1,4 +0,0 @@
-% initialize.m: a dummy routine needed for ps-files
-% GUISDAP v1.60   96-05-27 Copyright Asko Huuskonen, Markku Lehtinen
-%
-% This routine intentionally left dummy   < >
