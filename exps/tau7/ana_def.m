93715d1b7a413e0a693f283dc80adbb7b1aecd98
ingemar <>
2007-09-07 13:57:56 +0000

    tau7L fixes

--- exps/tau7/ana_def.m
+++ exps/tau7/ana_def.m
@@ -4,6 +4,7 @@ a_satch.clutter=[120];
 a_satch.repair=[Inf];
 a_satch.cut=1;
 d=find(analysis_altit>1200); analysis_altit(d(1))=1400;
-if name_site='L'
+if name_site=='L'
  analysis_maxwidth=3*analysis_maxwidth;
+ analysis_gating=2;
 end
