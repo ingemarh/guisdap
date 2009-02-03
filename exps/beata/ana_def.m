82ce5902258f40c38bdc32236180f9baea1d6e56
ingemar <>
2009-02-03 11:00:42 +0000

    Beata plasma line calib configured

--- exps/beata/ana_def.m
+++ exps/beata/ana_def.m
@@ -2,9 +2,17 @@
 %a_satch.sigma=4;
 %a_satch.plot=8;
 if name_site=='L'
-  a_satch.clutter=[19];
-  analysis_maxwidth=1.5*analysis_maxwidth;
-  analysis_ppshortlags=1;
+  if strfind(data_path,'@32p')
+    analysis_txpower=8;
+    analysis_intfixforce=[NaN NaN NaN 960];
+    analysis_intfix(5:6)=47:48;
+    analysis_plasmaline=1;
+    plasma_range=col(ones(2*125,1)*(0:1)*43509+(1:2*125)'*ones(1,2)+39259);
+  else
+    a_satch.clutter=[19];
+    analysis_maxwidth=1.5*analysis_maxwidth;
+    analysis_ppshortlags=1;
+  end
 elseif name_site=='T' | name_site=='V'
   a_satch.sigma=3;
   a_satch.cut=1;
--- exps/beata/pl_def.m
+++ exps/beata/pl_def.m
@@ -1,21 +1,31 @@
 %ran=[47 143;123 219;200 326];
 ran=[49 233;137 323;227 413];
-nfft=0; nint=1; ngates=3; nlag=25;
+nfft=0; nint=1; ngates=3;
 maxe=2; nup_d=1; skip_if=0;
-re=regexp(pl_dir,'\d\d\d\d-\d\d-\d\d_beata\d_\d+@[TV]p');
+re=regexp(pl_dir,'\d\d\d\d-\d\d-\d\d_beata\d_\d+@[TVL]p');
 if [strfind(expt,'v') strfind(expt,'V')]
  ran=2+ran;
  freq=[4.5 -4.5]*1e6; dt=0.8e-6; invert=-1; fradar=224e6;
- ele=90; updown=0:1;
+ ele=90; updown=0:1; nlag=25;
  if [strfind(expt,'cut') re]
-  startad=(0:1)*3*nlag+1;
+  startad=(0:1)*ngates*nlag+1;
  else
   startad=(0:1)*27907+32815+1;
  end
  %freq=freq(1); startad=startad(1); updown=0;
+elseif [strfind(expt,'l') strfind(expt,'L')]
+ ran=[42 266;155 379];
+ freq=[-4.0 4.0]*1e6; dt=0.4e-6; invert=-1; fradar=500e6;
+ ele=81.6; updown=0; nlag=125; ngates=2;
+ if [strfind(expt,'cut') re]
+  startad=(0:1)*ngates*nlag+1;
+ else
+  startad=(0:1)*39259+43509+1;
+ end
+ %freq=freq(1); startad=startad(1); updown=0;
 else
  freq=[-5.0]*1e6; dt=0.4e-6; invert=1; fradar=930e6;
- ele=77.5; updown=0;
+ ele=77.5; updown=0; nlag=50;
  if [strfind(expt,'cut') re]
   startad=0+1;
  else
