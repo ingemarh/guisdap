e9ce540a597e57f725bb8ba3961705386ac56c99
ingemar <>
2009-02-27 10:37:04 +0000

    Bugfix

--- anal/GUIDITOR.m
+++ anal/GUIDITOR.m
@@ -10,7 +10,7 @@ if name_site=='S' & d_time(1,1)==2003 & tosecs(d_time(1,:))>22809600 & tosecs(d_
   %correct a 10 kHz LO2 error in sept 2003 at Sodankyla
   r_phasepush=10e3;
 end
-if exists('a_phasepush','var')
+if exist('a_phasepush','var')
   r_phasepush=a_phasepush;
 end
 if isempty(r_phasepush) & d_date<datenum(2009,1,23) & ~isempty(strfind('KST',name_site))
