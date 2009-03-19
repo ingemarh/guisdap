317e4e05afc30b5a042c373bc135f4e8b6591e44
ingemar <>
2009-03-19 10:14:15 +0000

    Specifying 32bit ints in library interfaces

--- models/onera/matlab/onera_desp_lib.h
+++ models/onera/matlab/onera_desp_lib.h
@@ -21,11 +21,15 @@ prototype C header file for access to generic .dll or .so file
 e.g., onera_desp_lib.dll
 Paul O'Brien paul.obrien@aero.org
  */
+#ifdef __linux__
+#include <stdint.h>
+#else
+#include <sys/int_types.h>
+#endif
 
-
-void make_lstar1_(long int *ntime, long int *kext,
-		  long int *options,long int *sysaxes,
-		  long int *iyear,long int *idoy,
+void make_lstar1_(int32_t *ntime, int32_t *kext,
+		  int32_t *options,int32_t *sysaxes,
+		  int32_t *iyear,int32_t *idoy,
 		  double *UT,double *x1,
 		  double *x2,double *x3,
 		  double *maginput, double *Lm,
@@ -33,13 +37,13 @@ void make_lstar1_(long int *ntime, long int *kext,
 		  double *Bmin, double *J,
 		  double *MLT);
 
-void make_lstar_shell_splitting1_(long int *ntime, 
-				  long int *Nipa,
-				  long int *kext,
-				  long int *options,
-				  long int *sysaxes,
-				  long int *iyear,
-				  long int *idoy,
+void make_lstar_shell_splitting1_(int32_t *ntime, 
+				  int32_t *Nipa,
+				  int32_t *kext,
+				  int32_t *options,
+				  int32_t *sysaxes,
+				  int32_t *iyear,
+				  int32_t *idoy,
 				  double *UT,
 				  double *x1,
 				  double *x2,
@@ -53,9 +57,9 @@ void make_lstar_shell_splitting1_(long int *ntime,
 				  double *J,
 				  double *MLT);
 
-void drift_shell1_(long int *kext, long int *options,
-		   long int *sysaxes, long int *iyear,
-		   long int *idoy, double * UT,
+void drift_shell1_(int32_t *kext, int32_t *options,
+		   int32_t *sysaxes, int32_t *iyear,
+		   int32_t *idoy, double * UT,
 		   double *x1, double *x2, double *x3,
 		   double *maginput,
 		   double *Lm,
@@ -64,11 +68,11 @@ void drift_shell1_(long int *kext, long int *options,
 		   double *Bmin, 
 		   double *J,
 		   double *posit,
-		   long int *ind);
+		   int32_t *ind);
 
-void trace_field_line1_(long int *kext, long int *options,
-		   long int *sysaxes, long int *iyear,
-		   long int *idoy, double * UT,
+void trace_field_line1_(int32_t *kext, int32_t *options,
+		   int32_t *sysaxes, int32_t *iyear,
+		   int32_t *idoy, double * UT,
 		   double *x1, double *x2, double *x3,
 		   double *maginput,
 		   double *Lm,
@@ -76,21 +80,21 @@ void trace_field_line1_(long int *kext, long int *options,
 		   double *Bmin, 
 		   double *J,
 		   double *posit,
-		   long int *ind);
+		   int32_t *ind);
 
-void get_field1_(long int *kext, long int *options,
-		 long int *sysaxes,
-		 long int *iyear,long int *idoy,
+void get_field1_(int32_t *kext, int32_t *options,
+		 int32_t *sysaxes,
+		 int32_t *iyear,int32_t *idoy,
 		 double *UT,double *x1,
 		 double *x2,double *x3,
 		 double *maginput,
 		 double *Bgeo,
 		 double *B);
 
-void find_mirror_point1_(long int *kext, 
-			 long int *options,
-			 long int *sysaxes,
-			 long int *iyear,long int *idoy,
+void find_mirror_point1_(int32_t *kext, 
+			 int32_t *options,
+			 int32_t *sysaxes,
+			 int32_t *iyear,int32_t *idoy,
 			 double *UT,double *x1,
 			 double *x2,double *x3,
 			 double *alpha,
@@ -99,61 +103,61 @@ void find_mirror_point1_(long int *kext,
 			 double *Bmirror,
 			 double *xGEO);
 
-void find_magequator1_(long int *kext, 
-			 long int *options,
-			 long int *sysaxes,
-			 long int *iyear,long int *idoy,
+void find_magequator1_(int32_t *kext, 
+			 int32_t *options,
+			 int32_t *sysaxes,
+			 int32_t *iyear,int32_t *idoy,
 			 double *UT,double *x1,
 			 double *x2,double *x3,
 			 double *maginput,
 			 double *Bmin,
 			 double *xGEO);
 
-void get_mlt1_(long int *iyr, long int *idoy,
+void get_mlt1_(int32_t *iyr, int32_t *idoy,
 	       double *UT, double *xGEO, double *MLT);
 
-void fly_in_nasa_aeap1_(long int *ntime, long int *sysaxes,
-			long int *whichm, long int *whatf,
-			long int *Nene, double *energy, 
-			long int *iyear, long int *idoy, double *UT,
+void fly_in_nasa_aeap1_(int32_t *ntime, int32_t *sysaxes,
+			int32_t *whichm, int32_t *whatf,
+			int32_t *Nene, double *energy, 
+			int32_t *iyear, int32_t *idoy, double *UT,
 			double *x1,double *x2, double *x3,
 			double *flux);
 
-void get_ae8_ap8_flux_(long int *ntime, long int *whichm, long int *whatf,
-		       long int *Nene, double *energy, 
+void get_ae8_ap8_flux_(int32_t *ntime, int32_t *whichm, int32_t *whatf,
+		       int32_t *Nene, double *energy, 
 		       double *BBo, double *L, double *flux);
 
-void fly_in_afrl_crres1_(long int *ntime, long int *sysaxes,
-			 long int *whichm, long int *whatf,
-			 long int *nene, double *energy, 
-			 long int *iyear, long int *idoy, double *UT,
+void fly_in_afrl_crres1_(int32_t *ntime, int32_t *sysaxes,
+			 int32_t *whichm, int32_t *whatf,
+			 int32_t *nene, double *energy, 
+			 int32_t *iyear, int32_t *idoy, double *UT,
 			 double *x1, double *x2, double *x3,
 			 double *Ap15,
 			 double *flux,
 			 char *ascii_path,
-			 long int *strlen);
+			 int32_t *strlen);
 
-void get_crres_flux_(long int *ntime, long int *whichm, long int *whatf,
-		     long int *nene, double *energy, 
+void get_crres_flux_(int32_t *ntime, int32_t *whichm, int32_t *whatf,
+		     int32_t *nene, double *energy, 
 		     double *BBo,double *L, double *Ap15,
 		     double *flux,
 		     char *ascii_path,
-		     long int *strlen);
+		     int32_t *strlen);
 
-void sgp4_tle1_(long int *runtype,double *startsfe,double *stopsfe,double *deltasec,
-		char *InFileByte,long int *strlenIn,
-		char *OutFileByte,long int *strlenOut);
+void sgp4_tle1_(int32_t *runtype,double *startsfe,double *stopsfe,double *deltasec,
+		char *InFileByte,int32_t *strlenIn,
+		char *OutFileByte,int32_t *strlenOut);
 
-void sgp4_ele1_(long int *sysaxes,
-		long int *Yr,long int *Mon,long int *Day,long int *Hr,long int *Minute,double *Sec,
+void sgp4_ele1_(int32_t *sysaxes,
+		int32_t *Yr,int32_t *Mon,int32_t *Day,int32_t *Hr,int32_t *Minute,double *Sec,
 		double *e1, double *e2,	double *e3, double *e4,	double *e5, double *e6,
-		long int *ele_opts,
+		int32_t *ele_opts,
 		double *startsfe,double *stopsfe,double *deltasec,
-		long int *outYr,long int *outDoy, double *outSec,
+		int32_t *outYr,int32_t *outDoy, double *outSec,
 		double *x1, double *x2,	double *x3);
 
-void coord_trans_vec1_(long int *ntime, long int *sysaxesIN,long int *sysaxesOUT,
-		   long int *iyr,long int *idoy,double *secs,
+void coord_trans_vec1_(int32_t *ntime, int32_t *sysaxesIN,int32_t *sysaxesOUT,
+		   int32_t *iyr,int32_t *idoy,double *secs,
 		   double *xINV,double *xOUTV);
 
 void rv2coe_(double *R, double *V, 
@@ -161,20 +165,20 @@ void rv2coe_(double *R, double *V,
 	     double *Argp, double *Nu, double *M, double *ArgLat,
 	     double *TrueLon, double *LonPer);
 
-void fly_in_ige1_(long int *launch_year, long int *duration,
-		  long int *whichm, long int *whatf,
-		  long int *nene, double *energy,
+void fly_in_ige1_(int32_t *launch_year, int32_t *duration,
+		  int32_t *whichm, int32_t *whatf,
+		  int32_t *nene, double *energy,
 		  double *Lower_flux, double *Mean_flux, double *Upper_flux);
 
-void nrlmsise00_(long int *ntime,long int *whichAp,
-		long int *DOY,double *UT,double *ALT,double *LAT,double *LON,
+void nrlmsise00_(int32_t *ntime,int32_t *whichAp,
+		int32_t *DOY,double *UT,double *ALT,double *LAT,double *LON,
 		double *F107A,double *F107,double *AP,double *Dens,double *Temp);
 
-void msise90_(long int *ntime,long int *whichAp,
-		long int *DOY,double *UT,double *ALT,double *LAT,double *LON,
+void msise90_(int32_t *ntime,int32_t *whichAp,
+		int32_t *DOY,double *UT,double *ALT,double *LAT,double *LON,
 		double *F107A,double *F107,double *AP,double *Dens,double *Temp);
 
-void msis86_(long int *ntime,long int *whichAp,
-		long int *DOY,double *UT,double *ALT,double *LAT,double *LON,
+void msis86_(int32_t *ntime,int32_t *whichAp,
+		int32_t *DOY,double *UT,double *ALT,double *LAT,double *LON,
 		double *F107A,double *F107,double *AP,double *Dens,double *Temp);
 
