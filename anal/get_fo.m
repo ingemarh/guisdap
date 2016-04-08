83120b14fcea6f9bfbf6e5e5b6ef8c4f06a08ee1
eiscat <>
2016-04-08 12:22:24 +0000

    Carl-Fredrik Enell: Typo fix

--- anal/get_fo.m
+++ anal/get_fo.m
@@ -1,15 +1,42 @@
-function [day,fo]=get_fo(t1,t2,site)
+function [day,fo]=get_fo(t1,t2,site,epar,fpar)
+%
+% Retrieve DSND data from database
+% Update 20160408: E and F layer parameters selectable by user for new
+% (SQL) data.  
+% Default: foE and foF2. 
+% Suggested: sometimes FMXE and FMXF are better.
+%  
+% Usage: [day,fo]=get_fo(t1,t2,site,epar,fpar)
+% t1, t2: Time interval as Matlab datenum. Empty: default today
+% site: empty: default Tromsø | '(tromso|uhf|vfh)' | '(svalbard|32m|42m)'
+% epar: empty: default foE    |  1: FMXE           | '<parameter name>'
+% fpar: empty: default foF2   |  1: FMXF           | '<parameter name>'
+%
+% Last modified C-F Enell 20160408
+
 global local
+
 if nargin==0, t1=[]; end
 if isempty(t1), t1=now, end
+
 if nargin<2, t2=[]; end
 if isempty(t2), t2=t1; end
+
 if nargin<3, site='tromso'; end
 if findstr(site(1),'TV'), site='tromso'; end
 if site(1)=='L', site='svalbard'; end
 site3=site(1:3);
 if strcmp(site3,'32m') | strcmp(site3,'42m') | strcmp(site3,'esr'), site='svalbard'; end 
 if strcmp(site3,'uhf') | strcmp(site3,'vhf'), site='tromso'; end 
+
+if nargin<4, epar='foE'; end
+if epar==1, epar='FMXE'; end
+if isempty(epar), epar='foE'; end
+
+if nargin<5, fpar='foF2'; end
+if fpar==1, fpar='FMXF'; end
+if isempty(fpar), fpar='foF2'; end
+
 of=local.tfile;
 day=[]; fo=[];
 
@@ -20,7 +47,7 @@ if t1>datenum(2010,11,22)
  www='http://dynserv.eiscat.uit.no/';
  for t=fix(t1):fix(t2)
  tt=datevec(t); 
-  fof2_link=sprintf('DD/myque.php?q=select\\ dDay,foE,foF2\\ from\\ %s.resul%d_%02d_%02d\\ where\\ foE\\>0\\ or\\ foF2\\>0',site,tt(1:3));
+  fof2_link=sprintf('DD/myque.php?q=select\\ dDay,%s,%s\\ from\\ %s.resul%d_%02d_%02d\\ where\\ %s\\>0\\ or\\ %s\\>0',epar,fpar,site,tt(1:3),epar,fpar);
   [i,devnull]=system(sprintf(wget,www,fof2_link));
   if ~i
    form='%f%f%f%*[^\n]';
