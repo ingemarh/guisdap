9c0137fb21cbb9982d843b97aaf3f870afedba75
ingemar <>
2016-04-15 10:44:27 +0000

    Option to choose other parameters from the dynasond

--- anal/calib_ne.m
+++ anal/calib_ne.m
@@ -1,7 +1,8 @@
-function mr2=calib_ne(F,alt,maxe,minel,folim,polen,fpplt)
+function mr2=calib_ne(F,alt,maxe,minel,folim,polen,fpplt,ipar)
 %function mr2=calib_ne(F,alt,maxe,minel,folim,fpplt)
 %calib_ne.m: Utility to check the analysis against the dynasonde
 % GUISDAP v.8.3 04-05-27 Copyright EISCAT
+%  Changed 20160408, MTR to specify ipar, the ionosonde data type.
 % Inputs: F: 1 FoE, 2 FoF2 (default)
 %         alt: Altitude range, default 90-140, 180-500 resp.
 %         maxe: Maximum error allowed, default 1 (* std)
@@ -9,8 +10,11 @@ function mr2=calib_ne(F,alt,maxe,minel,folim,polen,fpplt)
 %         folim: Frequency range allowed, default [0 Inf]
 %         polen: Maximum length of polynomial fit, default 7, NaN=no fit
 %         fpplt: Indices to display the parabolic fits of plfs
+%         ipar: Ionosonde data type, 1 for asymptotic fit to crit freq (default)
+%               or 2 for fmfe or fmxf (peak pfreq from NeXtYZ)
 % Output: mr2: [Density_ratio Error NewMagic_const]
 global fpp_plot plf_polen
+if nargin<8, ipar=[]; epar=[]; fpar=[];  end
 if nargin<7, fpplt=[]; end
 if nargin<6, polen=[]; end
 if nargin<5, folim=[]; end
@@ -26,12 +30,16 @@ end
 if isempty(maxe), maxe=1; end
 if isempty(minel), minel=75; end
 if isempty(folim), folim=[0 30]; end
+if isempty(ipar), ipar=1; end
+if ipar == 1, epar='foE';, fpar='foF2';
+else epar='fmxe';, fpar='fmxf';
+end
 fpp_plot=fpplt;
 plf_polen=polen;
 a=vizu('verbose',alt,'P1 AE');
 global Time axs par1D DATA_PATH START_TIME END_TIME r_Magic_const name_ant
 d=datevec(Time(1));
-[dd,fo]=get_fo(Time(1),Time(end),name_ant);
+[dd,fo]=get_fo(Time(1),Time(end),name_ant,epar,fpar);
 t=[]; f=[];
 d=find(dd>datenum(START_TIME) & dd<datenum(END_TIME) & fo(:,F)>folim(1) & fo(:,F)<folim(2));
 dd=dd(d); fo=fo(d,:);
@@ -72,8 +80,12 @@ else
  rx=[0 ceil(max(col(f)))]; %rrx=((rx(1):.1:rx(2)));
  plot(f(good,1),f(good,2),'o',f(bad,1),f(bad,2),'o',rx,rx*sqrt(mr2),'-',rx,rx,'-');
  FO={'foE','foF2'};
+ FD=FO;
+ if ipar ~=1
+   FD={'fmxE','fmxF'};
+ end
  ylabel(sprintf('EISCAT %s (MHz)',char(FO(F))))
- xlabel(sprintf('Dynasonde %s (MHz)',char(FO(F))))
+ xlabel(sprintf('Dynasonde %s (MHz)',char(FD(F))))
  text(rx(2)*1.05,rx(2)/2,...
    sprintf('Density ratio=%.2f\\pm%.2f\nMagic const used=%g\n\nheight=%d-%d km\nelev > %g\\circ\ndynasonde window=%g-%g MHz\ngreen circles > %g\\sigma\n\nsuggested Magic const=%.2f',...
    mr2,sr2,Magic_const,alt,minel,folim,maxe,nmc),'horiz','left')
