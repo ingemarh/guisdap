652fea17d4d84cd33b3cb61f52b174f1e9045247
Carl-Fredrik Enell <carl-fredrik.enell@eiscat.se>
2017-03-08 11:57:48 +0100

    Correct windows-start.m template

--- windows-start.m
+++ windows-start.m
@@ -1,3 +1,21 @@
-addpath('C:\Users\Carl-Fredrik Enell\Desktop\guisdap8\anal')
-start_GUP
-analyse
+% Script for starting GUISDAP on Windows.
+%
+% Usage:
+% Browse to this file from the file explorer
+% or from a running Matlab GUI.
+%
+% *Righ-click* on the icon of this file.
+% Select Run.
+% Happy Guisdapping!
+%
+% -- remove this message
+disp('This script must be modified for *your* Windows path')
+disp('Edit and uncomment three lines')
+disp('(and remove this message)')
+% --
+%
+% --- modify and uncomment
+% addpath('C:\<MY\PATH\HERE>\guisdap8\anal')
+% start_GUP
+% analyse
+% ---
