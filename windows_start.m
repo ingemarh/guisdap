fe872c56857615ae9bf8c45baca71fd3ff176d1d
Carl-Fredrik Enell <carl-fredrik.enell@eiscat.se>
2017-03-10 08:19:07 +0000

    Corrected naming: hyphens in script names won't work on windows

---
+++ windows_start.m
@@ -0,0 +1,16 @@
+% Script for starting GUISDAP on Windows.
+%
+% Usage:
+% Browse to this file from the file explorer
+% or from a running Matlab GUI.
+%
+% *Righ-click* on the icon of this file.
+% Select Run.
+% Happy Guisdapping!
+%
+%
+% --- modify and uncomment
+addpath('C:\Users\fredrik\Desktop\guisdap8\anal')
+start_GUP
+analyse
+% ---
