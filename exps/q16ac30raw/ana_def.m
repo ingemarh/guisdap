cfb861090bac78538513937a2bbad7385dfe372a
ingemar <ingemar@lean.eiscat.se>
2017-05-30 21:57:24 +0200

    qujing raw analysis

---
+++ exps/q16ac30raw/ana_def.m
@@ -0,0 +1,11 @@
+f_ch_Pt=2e6;
+f_ch_el=84;
+f_ch_az=0;
+analysis_Tsys=150;
+%global iono_model
+%iono_model='gup150';
+analysis_txpower=8;
+f_calTemp=[];
+analysis_lagprofiling.par=load([path_expr 'q16x30.par']);
+%analysis_lagprofiling.lib='plwin';
+%analysis_lagprofiling.do=1;
