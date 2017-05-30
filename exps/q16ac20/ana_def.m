2f1c417b6cd03a11715e08527ef6a54e34f3e754
ingemar <ingemar@lean.eiscat.se>
2017-05-30 22:00:44 +0200

    20 us quing

---
+++ exps/q16ac20/ana_def.m
@@ -0,0 +1,9 @@
+f_ch_Pt=2e6;
+f_ch_el=84;
+f_ch_az=0;
+analysis_Tsys=150;
+%global iono_model
+%iono_model='gup150';
+analysis_txpower=8;
+f_calTemp=[];
+r0_setup=800e-6; %first range form setup
---
+++ exps/q16ac20/guispert.m
@@ -0,0 +1,5 @@
+if d_parbl(46)~=r0_setup
+ lpg_h=lpg_h-(r0_setup-d_parbl(46))*1e6/p_dtau;
+ r0_setup=d_parbl(46);
+ form_adpar
+end
