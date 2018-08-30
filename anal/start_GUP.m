d3e5129b9d2809de6dbc2c76d3cd085703f5ebd1
ingemar <ingemar@eiscat.se>
2018-08-30 09:20:27 +0200

    Start of vs 9.1

--- anal/start_GUP.m
+++ anal/start_GUP.m
@@ -10,7 +10,7 @@ clear all, clear global, clear functions
 
 global GUP_ver path_GUP path_exps path_tmp name_expr name_site data_path result_path local
 finish
-GUP_ver=9.0;
+GUP_ver=9.1;
 fprintf('GUISDAP vs. %g by EISCAT, Lehtinen&Huuskonen\n',GUP_ver)
 
 path_GUP=which('start_GUP','-all');
