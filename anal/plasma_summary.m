694e01342bc005833488562ca7cb3b633f6ce77a
ingemar <ingemar@eiscat.se>
2018-10-03 11:48:29 +0200

    Clear plasma_summary plot default

--- anal/plasma_summary.m
+++ anal/plasma_summary.m
@@ -7,7 +7,7 @@ function [varargout]=plasma_summary(pl_dir,printdir,expt,gates,plots)
 %        printdir Directory to save plot to (eps and png)
 %        expt   Name of experiment (or guessed from pl_dir)
 %        gates  Gates to display
-%        plots  Display a number of plots for internal checks
+%        plots  Display a number of plots for internal checks (-1, don't clear)
 % output: pl structure containing
 %            .t Time
 %            .s Spectra
@@ -22,6 +22,7 @@ elseif gates==0
 end
 gate=[];
 if nargin<5, plots=[]; end
+if isempty(plots) | plots~=-1, clf, plots=[]; end
 if isempty(expt)
  [i,expt]=fileparts(pl_dir);
  if isempty(expt), [i,expt]=fileparts(i); end
