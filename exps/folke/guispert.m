7e04f1a8cc7dbd0c6baa9be472c8d334ebd7182e
kstdev <ingemar@eiscat.se>
2019-10-10 12:12:18 +0000

    New 42m direction

--- exps/folke/guispert.m
+++ exps/folke/guispert.m
@@ -5,12 +5,15 @@
 %
 if expver==1 && a_code(1)==3 || a_code(1)==2
  name_ant='42m'; calTemp=163; ch_gain=10^4.52;
- if d_date>datenum(2009,9,10)
+ if d_date>datenum(2019,10,09)
+  ch_el=82.1; ch_az=185.5;
+ elseif d_date>datenum(2016,12,16)
+  ch_el=81.6; ch_az=185.5;
+ elseif d_date>datenum(2009,9,10)
   ch_el=81.6; ch_az=184.5;
  else
   ch_el=81.6; ch_az=182.1;
  end
- ch_gain=10^4.52;
  if expver==1
   glp=1436;
   grps=[1438 1438 lpg_h(1438);1439 2307 lpg_h(1439)+lpg_w(1439)/2
--- exps/hilde/guispert.m
+++ exps/hilde/guispert.m
@@ -5,6 +5,15 @@
 %
 %lpg_bcs([1417 1927])='x';
 if a_code(1)==1
- name_ant='42m'; ch_el=81.6; ch_az=182.1; calTemp=163;
+ name_ant='42m'; calTemp=163;
+ if d_date>datenum(2019,10,09)
+  ch_el=82.1; ch_az=185.5;
+ elseif d_date>datenum(2016,12,16)
+  ch_el=81.6; ch_az=185.5;
+ elseif d_date>datenum(2009,9,10)
+  ch_el=81.6; ch_az=184.5;
+ else
+  ch_el=81.6; ch_az=182.1;
+ end
  ch_gain=10^4.52;
 end
