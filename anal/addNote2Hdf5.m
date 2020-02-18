3e227937834cf318eff7ea29b1e747e74ac0cb96
Rikard Slapak <rikard.slapak@eiscat.se>
2020-02-18 15:27:04 +0100

    Modyfying addNotes2Hdf5.m

--- anal/addNote2Hdf5.m
+++ anal/addNote2Hdf5.m
@@ -1,6 +1,7 @@
 function addNote2Hdf5(notefile,hdf5file,N)
 %addNote2Hdf5(notefile,hdf5file) adds any text (comments, notes etc) in 'notefile' 
 %to 'hdf5file'in /metadata/commentN, where N is a number (left out if N = 1) 
+
 if nargin<3
     N = 1;
 end
@@ -9,26 +10,17 @@ if nargin<2
 end
 
 fid = fopen(notefile);
-text = textscan(fid,'%s');
-text_cells = text{1};
+notes = textscan(fid,'%s','delimiter','');
+notes_lines = notes{1};
 notes_str = [];
-for nc = 1:length(text_cells)
-     if nc == length(text_cells)
-         notes_str = [notes_str char(text_cells(nc))];
+for nc = 1:length(notes_lines)
+     if nc == length(notes_lines)
+         notes_str = [notes_str char(notes_lines(nc))];
      else
-         notes_str = [notes_str char(text_cells(nc)) ' '];
+         notes_str = [notes_str char(notes_lines(nc)) newline];
      end
 end
 
-notes_str = strrep(notes_str,'<BR>',newline);
-notes_str = erase(notes_str,["<B>","</B>","<a","</a>","href=",">"]);
+notes_str = erase(notes_str,["<BR>","<B>","</B>"," <a","</a>","href=",">"]);
 notes_str = regexprep(notes_str,'"','');
-      
-if N == 1
-    strds2hdf5(hdf5file,'/metadata','comments',{notes_str})
-else
-    strds2hdf5(hdf5file,'/metadata',['comments' num2str(N)],{notes_str})
-end
-
-end
-
+strds2hdf5(hdf5file,'/metadata','comments',{notes_str})
