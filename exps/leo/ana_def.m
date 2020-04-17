875020624c03595b58c356e590c6f034357cb592
Ingemar <ingemar@lean.eiscat.se>
2020-04-17 10:53:11 +0200

    Some leo tries

--- exps/leo/ana_def.m
+++ exps/leo/ana_def.m
@@ -6,9 +6,10 @@ analysis_lpf.raw=[];
 analysis_lpf.data=0;
 analysis_lpf.nsamp=20000;
 analysis_lpf.p=[0 128*5-1]; %start/stop profiles to analyse
-%analysis_lpf(2).par=load(['leo_u.par']);
 analysis_lpf(2).par=load([path_expr 'leo_u.par']);
 analysis_lpf(2).lib='plwin';
 analysis_lpf(2).raw=134*128;
 analysis_lpf(2).p=[0 128*5-1]; %start/stop profiles to analyse
 analysis_lpf(2).data=14;
+
+analysis_altit=80+(0:3:700^.7).^1.4;
