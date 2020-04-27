c54624610985f39eca8ba30eba71ffe6a651a320
Ingemar <ingemar@lean.eiscat.se>
2020-04-27 10:11:53 +0200

    Added unit to R2H and H2R calcs

--- anal/height_to_range.m
+++ anal/height_to_range.m
@@ -5,16 +5,20 @@
 % function that calculates the range of a volume (in us)
 % when the height (in km) and antenna direction (in deg) is known.
 % assumes spherical earth for the moment
+% optional unit for other units
 %
 % See also: range_to_height
 %
-% function range=height_to_range(height,el)
-  function range=height_to_range(height,el)
-  
+% function range=height_to_range(height,el,unit)
+function range=height_to_range(height,el,unit)
+Earth_radius=6372e3;
+sin_el=sin(pi*el/180);
+if nargin<3
+  unit=1e3;
   global p_dtau v_lightspeed
-  
-  Earth_radius=6372;
-  hei=height/Earth_radius;
-  sin_el=sin(pi*el/180);
-  range=1000*Earth_radius*(sqrt(sin_el.^2+2*hei+hei.^2)-sin_el);
-  range=range/(p_dtau*1e-6*v_lightspeed/2);
+  fac=p_dtau*1e-6*v_lightspeed/2/unit;
+else
+  fac=1;
+end
+hei=height*unit/Earth_radius;
+range=Earth_radius/unit/fac*(sqrt(sin_el.^2+2*hei+hei.^2)-sin_el);
--- anal/range_to_height.m
+++ anal/range_to_height.m
@@ -2,18 +2,23 @@
 % GUISDAP v.1.60 96-05-27 Copyright Asko Huuskonen and Markku Lehtinen
 %
 % when the range (in us) and antenna direction (in deg) is known.
+% optional unit for other units
 %
 % See also: loc2gg height_to_range
 %
-% function height=range_to_height(range,el)
-  function height=range_to_height(range,el)
-  
+% function height=range_to_height(range,el,unit)
+function height=range_to_height(range,el,unit)
+Earth_radius=6372e3;
+sin_el=sin(pi*el/180);
+if nargin<3
+  unit=1e3;
   global p_dtau v_lightspeed
-  
-  Earth_radius=6372;
-  ran=range*(p_dtau*1e-6*v_lightspeed/2)/1000/Earth_radius;
-  sin_el=sin(pi*el/180);
-  height=Earth_radius*(sqrt(1+2*ran*sin_el+ran.^2)-1);
+  fac=p_dtau*1e-6*v_lightspeed/2/unit;
+else
+  fac=1;
+end
+ran=range*unit/Earth_radius*fac;
+height=Earth_radius/unit*(sqrt(1+2*ran*sin_el+ran.^2)-1);
 
 % This code for elliptical earth
 %  scale=(p_dtau*1e-6*v_lightspeed/2);
