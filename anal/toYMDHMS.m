ce99e6283a9e331a71a9d9ae30b8bec2dea0902e
Ingemar <ingemar@lean.eiscat.se>
2020-05-13 15:09:49 +0200

    General time conversion

--- anal/toYMDHMS.m
+++ anal/toYMDHMS.m
@@ -6,32 +6,22 @@
 % to form [Year Month Day Hour Min Sec]
 % If mask is given, those elements will be returned in a character
 % string, with separator 'sep' if specified
+% Leap second information
 %
-% See also: tosecs
+% See also: tosecs utc2tai
 % function YMD=toYMDHMS(years,secs,mask,sep)
-  function YMD=toYMDHMS(years,secs,mask,sep)
+function [YMD,leap]=toYMDHMS(years,secs,mask,sep)
 
 secs=col(secs);
-if length(years)==1 years=years*ones(size(secs));
-else               years=col(years); end
-
-  days=cumsum([0 31 28 31 30 31 30 31 31 30 31 30 31])';
-  days=days(:,ones(1,length(years)));
-  ind=find(rem(years,4)==0);
-  if length(ind)>0,days(3:13,ind)=days(3:13,ind)+1;end
-
-  YMD(:,6)=rem(secs,60); apu=floor(secs/60);
-  YMD(:,5)=rem(apu,60); apu=floor(apu/60);
-  YMD(:,4)=rem(apu,24); apu=floor(apu/24)+1; 
-  index=sum((apu(:,ones(1,13))'-days)>0);
-  YMD(:,2)=index';
-  YMD(:,3)=apu-days(YMD(:,2),1);
-  YMD(:,1)=years;
+if length(years)==1
+ years=years*ones(size(secs));
+else
+ years=col(years);
+end
+[YMD,leap]=utc2tai([secs years],'gup2utc');
 
 if nargin==2 return, end
-
 if nargin==3 sep=':';end
-
 YMD(:,1)=YMD(:,1)-100*floor(YMD(:,1)/100);
 fmt='%0.2d';
 for i=1:length(mask)-1 fmt=[fmt sep '%0.2d']; end
--- anal/utc2tai.m
+++ anal/utc2tai.m
@@ -1,27 +1,27 @@
-
 function [date,leaps] = utc2tai(date0,direction)
 % [date,leaps] = utc2tai(date0,direction)
 %
 % date: date output in new format
 % leaps: leap seconds between the output and input time formats
 %
-% Convert between different time formats (utc --> tai), or vice versa (tai --> utc)
+% Convert between different time formats utc|tai|mat|unx
 % 
 % date0:     input time to be converted
 % direction: specifies the time formats of the input and output. 
 %            Possible options:
-%            1: 'utc2tai'
-%            2: 'tai2utc'
-%  
+%            1: '(utc|mat|unx|gup)2tai' 
+%            2: 'tai2(utc|mat|unx|gup)'
 % Currently step times are through Jan 1 2017, but need to be added in the
 % function list as they are instuted. The first addition of leapseconds (10 s) was at 
 % 'Jan 1 1972'. Following dates correspond to additions of new individual
 % leapseconds.
-%   
-if nargin < 2
-    error('Error: direction was not specified, define format of time input and output.')
+if nargin<2
+    direction='utc2tai';
 end
-if ~contains('utc2tai tai2utc',direction)
+f='utc mat unx tai gup';
+dir=split(direction,'2');
+unx0=datenum(1970,1,1);
+if ~contains(f,dir{1}) &&  length(dir)>1 && ~contains(f,dir{2})
     error(['Error: ' direction ' is not a possible convert direction. Misspelled?'])
 end
 
@@ -57,30 +57,49 @@ stepdates = [...
     'Jan 1 2017'];
 %% Convert Steps to datenums and make step offsets
 stepdates = datenum(stepdates)'; %step date conversion
-steptime = ((0:length(stepdates)-1)'+10)./86400; %corresponding step time (sec)
+step=[10;ones(length(stepdates)-1,1)]'/86400;
+steptime=cumsum(step);
+ns=length(stepdates);
 %% Arg Checking
-if ~isnumeric(date0) %make sure date0 are datenums, if not try converting
-    date0 = datenum(date0); %will error if not a proper format
-end
-%%
-if date0<stepdates(1)
-    leaps = 0;
-    date = date0;
-    return
+if contains('unxtai',dir{1})
+  date0=date0/86400+unx0;
+elseif contains('gup',dir{1})
+  date0=date0(:,1)/86400+datenum(date0(:,2),1,1);
+else
+  date0=datenum(date0); %will error if not a proper format
 end
 %% Array Sizing
-sz = size(date0);
-date0 = date0(:);
-date0 = repmat(date0,[1 size(stepdates,2)]);
-stepdates = repmat(stepdates,[size(date0,1) 1]);
+date0=date0(:);
+nt=size(date0,1);
+date=date0;
+leaps=zeros(nt,1);
+d=find(date0>stepdates(1));
+ld=length(d);
+date0=repmat(date0(d),1,ns);
+stepdates=repmat(stepdates,ld,1);
 %% Conversion
-leapsec_inday = steptime(sum((date0 - stepdates) >= 0,2)); 
-leaps = leapsec_inday*86400;
-if strcmp(direction,'utc2tai')
-    date = date0(:,1) + leapsec_inday;
-elseif strcmp(direction,'tai2utc')
-    date = date0(:,1) - leapsec_inday;
+leapsec_inday=steptime(sum((date0-stepdates)>=0,2));
+leaps(d)=leapsec_inday*86400;
+if strcmp(dir{2},'tai')
+  date(d)=date0(:,1)+leapsec_inday;
 end
-%% Reshape Output Array
-date = reshape(date,sz);
+if strcmp(dir{1},'tai')
+  date(d)=date0(:,1)-leapsec_inday;
+end
+if contains('unxtai',dir{2})
+  date=(date-unx0)*86400;
+elseif contains('gup',dir{2})
+  dated=datevec(date);
+  date=[dated(:,1) (date-datenum(dated,1,1))*86400];
+elseif contains('utc',dir{2})
+  if strcmp(dir{1},'tai')
+    step0=repmat(step,ld,1);
+    [i,j]=find(date0>=stepdates & date0<(stepdates+step0));
+    date(d(i))=date(d(i))-1;
+    date=datevec(date);
+    li=length(i);
+    date(d(i),4:6)=[repmat([23 59 60],li,1)+[zeros(li,2) [date(d(i),6)+step0(j)]]];
+  else
+    date=datevec(date);
+  end
 end
